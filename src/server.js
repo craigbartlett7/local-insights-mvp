a8e48fc9b6580db65926f95e525d381b4a688703
craigbartlett7 <craigbartlett7@gmail.com>
2025-08-27 16:45:39 +0100

    Add files via upload
    2.7 patch fix start

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
+import 'dotenv/config';
 console.log('skeleton');
+
+process.on('unhandledRejection', (reason) => {
+  console.error('UnhandledRejection:', reason);
+});
+process.on('uncaughtException', (err) => {
+  console.error('UncaughtException:', err);
+});
